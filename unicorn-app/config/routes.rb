682c17c8f5a4298574c1fbd9e1779b2973bc1bd5
Shouta Kimura <kimura-shota@kayac.com>
2015-12-14 23:21:58 +0900

    scaffold を削除

--- unicorn-app/config/routes.rb
+++ unicorn-app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :boards
+  get 'welcome/index'
+
   root "boards#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
